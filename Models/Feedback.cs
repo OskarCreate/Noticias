2af810e7fbc2c4e58b72d8684e0547ee3ff20a22
OskarCreate <oskar_morales_f@outlook.com>
2025-05-23 22:27:10 -0500

    Cambios del modelo

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,12 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics.CodeAnalysis;public class Feedback
+using System.Diagnostics.CodeAnalysis;
+public class Feedback
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+
     public int PostId { get; set; }
+
     public string? Sentimiento { get; set; }
+
     public DateTime Fecha { get; set; }
 }
 
